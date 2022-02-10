5a6379d47217dcc0f8a4b50ddfa3021a107382f9
prateek2712 <47187002+prateek2712@users.noreply.github.com>
2022-02-10 23:05:21 +0530

    code update

--- 3_Implementation/code.c
+++ 3_Implementation/code.c
@@ -3,33 +3,12 @@
 #include <math.h>
 #include <ctype.h>
 #include<string.h>
+#include "header.h"
 #define pi 3.1415
 
-float convertmm(char *unit, float value)
-{
 
-    if (unit == "cm")
-        return value * 10;
-    else if (unit == "m")
-        return value * 1000;
-    else if (unit == "in")
-        return value * 25.4;
-    else if (unit == "ft")
-        return value * 25.4 * 12;
-    else
-        return value;
-}
 
-float metal_dens(char *m_t)
-{
-    if (*m_t == 'M' || *m_t == 'm')
-        return 7850;
-    if (*m_t == 'C' || *m_t == 'c')
-        return 7200;
-    if (*m_t == 'A' || *m_t == 'a')
-        return 2700;
-    return 0;
-}
+
 
 void main()
 {
